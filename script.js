b187a5e12faab89bc37a3b6d337c20707e104d60
Md. Azizul Hakim <aazizul924@gmail.com>
2026-07-13 16:56:22 +0600

    final commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const apiKey = "536edfe8204282d316a6eae9efb88fb5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
+
+        const windSpeedMs = data.wind.speed;
+        const windSpeedKmh = (windSpeedMs * 3.6).toFixed(1);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = windSpeedKmh + " km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "/images/cloudy-2.gif"
+        }
+        else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "/images/clear.gif"
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "/images/rain.gif"
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "/images/drizzle.gif"
+        }
+        else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "/images/mist.png"
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+
+    }
+
+
+};
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+    searchBox.value = "";
+})
