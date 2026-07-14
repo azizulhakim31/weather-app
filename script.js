21ae17ed18fa8ceaddfba2749391b8224327f33a
Md. Azizul Hakim <aazizul924@gmail.com>
2026-07-14 10:46:51 +0600

    issue solved

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ async function checkWeather(city) {
         document.querySelector(".weather").style.display = "none";
     } else {
         var data = await response.json();
-
         const windSpeedMs = data.wind.speed;
         const windSpeedKmh = (windSpeedMs * 3.6).toFixed(1);
 
@@ -23,19 +22,19 @@ async function checkWeather(city) {
         document.querySelector(".wind").innerHTML = windSpeedKmh + " km/h";
 
         if (data.weather[0].main == "Clear") {
-            weatherIcon.src = "/assets/clear.gif"
+            weatherIcon.src = "assets/clear.gif"
         }
         else if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = "/assets/cloudy-2.gif"
+            weatherIcon.src = "assets/cloudy-2.gif"
         }
         else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = "/assets/rain.gif"
+            weatherIcon.src = "assets/rain.gif"
         }
         else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "/assets/drizzle.gif"
+            weatherIcon.src = "assets/drizzle.gif"
         }
         else if (data.weather[0].main == "Mist") {
-            weatherIcon.src = "/assets/mist.png"
+            weatherIcon.src = "assets/mist.png"
         }
 
         document.querySelector(".weather").style.display = "block";
